97b034ea1754cb173f7398eff1d5817a972d7a4a
peixotorms <raul.peixoto@gmail.com>
2018-02-25 21:27:36 +0000

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SOURCE="$1"
+if [ "${SOURCE}" == "" ]; then
+    echo "Must specify a source url"
+    exit 1
+fi
+
+FILEID=$(echo $SOURCE | rev | cut -d= -f1 | rev)
+COOKIES=$(mktemp)
+
+CODE=$(wget --save-cookies $COOKIES --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=${FILEID}" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/Code: \1\n/p')
+
+# cleanup the code, format is 'Code: XXXX'
+CODE=$(echo $CODE | rev | cut -d: -f1 | rev | xargs)
+
+wget --load-cookies $COOKIES --content-disposition "https://docs.google.com/uc?export=download&confirm=${CODE}&id=${FILEID}"
+
+rm -f $COOKIES
